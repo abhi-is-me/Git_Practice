a406bc599ce954b50df86058a143c8530c22b8b4
abhis-is-me <abhishekbmwblowhorn@gmail.com>
2024-11-09 12:03:31 +0530

    Added dtos

--- WebApi_practice/obj/Debug/net8.0/WebApi_practice.AssemblyInfo.cs
+++ WebApi_practice/obj/Debug/net8.0/WebApi_practice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi_practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae96f292b4832a167015a6f234e18aedaae681b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi_practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi_practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
