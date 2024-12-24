63b6959c8e32476456f37bfeca42b1b396258187
abhis-is-me <abhishekbmwblowhorn@gmail.com>
2024-12-24 19:20:30 +0530

    unwnated changes

---
+++ WebApi_practice/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi_practice.Models
+{
+    public class Employee
+    {
+        public Employee() { }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
